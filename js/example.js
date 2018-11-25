b41e6bf6c750bb9eae407653d3726ebcc0ccba36
drinkNplay <45202873+drinkNplay@users.noreply.github.com>
2018-11-25 16:22:18 +0000

    Update example.js

--- js/example.js
+++ js/example.js
@@ -13,19 +13,19 @@ deck.addCards(cards.all);
 deck.render({immediate:true});
 
 //Now lets create a couple of hands, one face down, one face up.
-upperhand = new cards.Hand({faceUp:false, y:60});
-lowerhand = new cards.Hand({faceUp:true, y:340});
+upperhand = new cards.Hand({faceUp:true, y:60});
+//lowerhand = new cards.Hand({faceUp:true, y:340});
 
 //Lets add a discard pile
 discardPile = new cards.Deck({faceUp:true});
-discardPile.x += 50;
+discardPile.x += 100;
 
 
 //Let's deal when the Deal button is pressed:
 $('#deal').click(function() {
 	//Deck has a built in method to deal to hands.
 	$('#deal').hide();
-	deck.deal(5, [upperhand, lowerhand], 50, function() {
+	deck.deal(4, [upperhand, lowerhand], 50, function() {
 		//This is a callback function, called when the dealing
 		//is done.
 		discardPile.addCard(deck.topCard());
