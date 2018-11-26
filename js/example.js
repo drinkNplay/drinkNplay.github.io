3e4bc054e2939b146ceca2530825a355aca3ecf1
drinkNplay <45202873+drinkNplay@users.noreply.github.com>
2018-11-26 00:46:14 +0000

    Update example.js

--- js/example.js
+++ js/example.js
@@ -1,3 +1,17 @@
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://192.168.1.68', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'task_queue';
+    var msg = process.argv.slice(2).join(' ') || "Hello World!";
+
+    ch.assertQueue(q, {durable: true});
+    ch.sendToQueue(q, new Buffer(msg), {persistent: true});
+    console.log(" [x] Sent '%s'", msg);
+  });
+  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+});
+
 //Tell the library which element to use for the table
 cards.init({table:'#card-table'});
 
