1beae93d29efc614aadc77fddb3db98de9621d0f
drinkNplay <45202873+drinkNplay@users.noreply.github.com>
2018-11-26 00:12:35 +0000

    Update exemplo2.js

--- js/exemplo2.js
+++ js/exemplo2.js
@@ -3,25 +3,15 @@ cards.init({table:'#card-table'});
 
 lowerhand = new cards.Hand({faceUp:true, y:-100});
 
-window.onload =  deck.deal(4, [lowerhand], 50, function() {
+$('#deal').click(function() {
+    deck.deal(4, [lowerhand], 50, function() {
         //This is a callback function, called when the dealing
         //is done.
         discardPile.addCard(deck.topCard());
         discardPile.render();
+    });
 });
 
-//When you click on the top card of a deck, a card is added
-//to your hand
-deck.click(function(card){
-    if (card === deck.topCard()) {
-        lowerhand.addCard(deck.topCard());
-        lowerhand.render();
-    }
-});
-
-//Finally, when you click a card in your hand, if it's
-//the same suit or rank as the top card of the discard pile
-//then it's added to it
 lowerhand.click(function(card){
     if (card.suit == discardPile.topCard().suit
         || card.rank == discardPile.topCard().rank) {
@@ -30,3 +20,4 @@ lowerhand.click(function(card){
         lowerhand.render();
     }
 });
+
